ed3f9b505bc6cb1e22154eacc199886ab6404d8e
Lijun Wang <lijun@solana.com>
2021-07-22 01:30:08 -0700

    Renamed the module names

--- rpc-repl-lib/build.rs
+++
@@ -1,8 +0,0 @@
-use tonic_build;
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // compiling protos using path on build time
-    tonic_build::configure()
-        .compile(&["proto/accountsdb_repl.proto"], &["proto"])?;
-    Ok(())
-}
