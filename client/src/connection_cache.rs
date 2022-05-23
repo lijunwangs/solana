9473dc2d3771f410afd757163c68348af7cfb478
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-05-23 00:09:30 -0700

    Add reference when checking out a connection from cache

--- client/src/connection_cache.rs
+++ client/src/connection_cache.rs
@@ -377,6 +377,7 @@ fn get_or_add_connection(addr: &SocketAddr) -> GetConnectionResult {
                 } else {
                     let mut rng = thread_rng();
                     let n = rng.gen_range(0, connections.0.len());
+                    connections.1.fetch_add(1, Ordering::Relaxed);
                     let connection = connections.0[n].clone();
                     (connection, true, map.stats.clone(), 0, 0)
                 }
