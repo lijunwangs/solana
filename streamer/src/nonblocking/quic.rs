21faa25330a014cbc379c520cf3a451372bbf55c
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-09-27 15:03:23 -0700

    increased timeout again

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -519,7 +519,7 @@ async fn handle_connection(
                             let mut maybe_batch = None;
                             while !stream_exit.load(Ordering::Relaxed) {
                                 if let Ok(chunk) = tokio::time::timeout(
-                                    Duration::from_millis(1000),
+                                    Duration::from_millis(10),
                                     stream.read_chunk(PACKET_DATA_SIZE, false),
                                 )
                                 .await
