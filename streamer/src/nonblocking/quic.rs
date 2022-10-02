0df7d0d45544d58bcab43e2d5dd74e79d2bd390f
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-10-02 16:52:27 -0700

    Increase stream timeout

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -546,7 +546,7 @@ async fn handle_connection(
                             while !stream_exit.load(Ordering::Relaxed) {
                                 if let Ok(chunk) = tokio::time::timeout(
                                     //Duration::from_millis(WAIT_FOR_STREAM_TIMEOUT_MS),
-                                    Duration::from_millis(10000),
+                                    Duration::from_millis(200000),
                                     stream.read_chunk(PACKET_DATA_SIZE, false),
                                 )
                                 .await
