08020a33148a39e92a2e66e0b373b20183442589
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2022-10-05 16:14:32 -0700

    Add debug message about forward

--- core/src/banking_stage.rs
+++ core/src/banking_stage.rs
@@ -628,6 +628,7 @@ impl BankingStage {
                     .forwarded_transaction_count
                     .fetch_add(packet_vec_len, Ordering::Relaxed);
                 let conn = connection_cache.get_connection(&addr);
+                info!("Forwarding to the new leader: via quic_client {}", addr);
                 conn.send_wire_transaction_batch_async(packet_vec)
             };
 
