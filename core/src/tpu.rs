0f28430aa5bd5b2efc35c68329c5db9e602e3717
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2023-04-17 14:28:02 -0700

    max connections per peer fot test;

--- core/src/tpu.rs
+++ core/src/tpu.rs
@@ -44,7 +44,7 @@ use {
 pub const DEFAULT_TPU_COALESCE_MS: u64 = 5;
 
 // allow multiple connections for NAT and any open/close overlap
-pub const MAX_QUIC_CONNECTIONS_PER_PEER: usize = 8;
+pub const MAX_QUIC_CONNECTIONS_PER_PEER: usize = 2500;
 
 pub struct TpuSockets {
     pub transactions: Vec<UdpSocket>,
