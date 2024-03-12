899a83798c9f80d234e292b3f69016a48ed336f8
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-03-12 15:14:23 -0700

    clippy issue

--- programs/sbf/tests/programs.rs
+++ programs/sbf/tests/programs.rs
@@ -112,6 +112,7 @@ fn process_transaction_and_record_inner(
             },
             &mut ExecuteTimings::default(),
             None,
+            None,
         )
         .0;
     let result = results
@@ -676,7 +677,7 @@ fn test_return_data_and_log_data_syscall() {
         let blockhash = bank.last_blockhash();
         let message = Message::new(&[instruction], Some(&mint_keypair.pubkey()));
         let transaction = Transaction::new(&[&mint_keypair], message, blockhash);
-        let sanitized_tx = SanitizedTransaction::from_transaction_for_tests(transaction);
+        let sanitized_tx = SanitizedTransaction::from_transaction_for_tests(transaction).into();
 
         let result = bank.simulate_transaction(&sanitized_tx, false);
 
