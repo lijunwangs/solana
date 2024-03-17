582c597e71c1a8adc2bd8bbda22f15d73c3ae2e7
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-03-17 11:26:53 -0700

    add metrics on throttled staked vs non-staked

--- streamer/src/quic.rs
+++ streamer/src/quic.rs
@@ -179,6 +179,8 @@ pub struct StreamStats {
     pub(crate) perf_track_overhead_us: AtomicU64,
     pub(crate) total_staked_packets_sent_for_batching: AtomicUsize,
     pub(crate) total_unstaked_packets_sent_for_batching: AtomicUsize,
+    pub(crate) throttled_staked_streams: AtomicUsize,
+    pub(crate) throttled_unstaked_streams: AtomicUsize,
 }
 
 impl StreamStats {
@@ -434,6 +436,7 @@ impl StreamStats {
                 i64
             ),
             (
+
                 "stream_load_ema",
                 self.stream_load_ema.load(Ordering::Relaxed),
                 i64
@@ -446,6 +449,16 @@ impl StreamStats {
             (
                 "stream_load_capacity_overflow",
                 self.stream_load_capacity_overflow.load(Ordering::Relaxed),
+                i64
+            ),
+            (
+                "throttled_unstaked_streams",
+                self.throttled_unstaked_streams.swap(0, Ordering::Relaxed),
+                i64
+            ),
+            (
+                "throttled_staked_streams",
+                self.throttled_staked_streams.swap(0, Ordering::Relaxed),
                 i64
             ),
             (
