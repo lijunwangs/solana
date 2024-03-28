616c3c56e411f3295b8bcef61df09ec4cf3fb396
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-03-28 12:13:39 -0700

    turn on rate limiting

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -193,7 +193,7 @@ async fn run_server(
                 .fetch_add(1, Ordering::Relaxed);
             let remote_address = connection.remote_address();
             debug!("Got a connection {:?}", remote_address);
-            let do_rate_limiting = false;
+            let do_rate_limiting = true;
             if do_rate_limiting && !rate_limiter.check(&remote_address.ip()) {
                 debug!("Reject attacker connection from {:?}", remote_address);
                 stats.connection_throttled.fetch_add(1, Ordering::Relaxed);
