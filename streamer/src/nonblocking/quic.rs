d889daa997e548b16f129f69fd4112e14a12482a
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-04-10 14:31:37 -0700

    address some comment from Pankaj

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -2,7 +2,7 @@ use {
     crate::{
         nonblocking::stream_throttle::{
             ConnectionStreamCounter, StakedStreamLoadEMA, MAX_STREAMS_PER_MS,
-            STREAM_STOP_CODE_THROTTLING,
+            STREAM_STOP_CODE_THROTTLING, STREAM_THROTTLING_INTERVAL_MS,
         },
         quic::{configure_server, QuicServerError, StreamStats},
         streamer::StakedNodes,
@@ -501,18 +501,18 @@ async fn setup_connection(
                         stats.clone(),
                     ),
                     |(pubkey, stake, total_stake, max_stake, min_stake)| {
-                        let peer_type = if stake > 0 {
+                        // The heuristic is that the stake should be large engouh to have 1 stream pass throuh within one throttle
+                        // interval during which we allow max (MAX_STREAMS_PER_MS * STREAM_THROTTLING_INTERVAL_MS) streams.
+                        let min_stake_ratio =
+                            1_f64 / (MAX_STREAMS_PER_MS * STREAM_THROTTLING_INTERVAL_MS) as f64;
+                        let stake_ratio = stake as f64 / total_stake as f64;
+                        let peer_type = if stake_ratio < min_stake_ratio {
                             // If it is a staked connection with ultra low stake ratio, treat it as unstaked.
-                            let min_ratio = 1_f64 / (MAX_STREAMS_PER_MS * 100) as f64;
-                            let stake_ratio = stake as f64 / total_stake as f64;
-                            if stake_ratio < min_ratio {
-                                ConnectionPeerType::Unstaked
-                            } else {
-                                ConnectionPeerType::Staked(stake)
-                            }
-                        } else {
                             ConnectionPeerType::Unstaked
+                        } else {
+                            ConnectionPeerType::Staked(stake)
                         };
+
                         NewConnectionHandlerParams {
                             packet_sender,
                             remote_pubkey: Some(pubkey),
