a90fc19fd3596cd708ecd73799ebc9faf39dedc3
Lijun Wang <lijun.wang@oracle.com>
2024-06-19 19:04:37 +0900

    revert DEFAULT_TPU_ENABLE_UDP change in local_cluster

--- local-cluster/src/local_cluster.rs
+++ local-cluster/src/local_cluster.rs
@@ -337,7 +337,7 @@ impl LocalCluster {
             socket_addr_space,
             DEFAULT_TPU_USE_QUIC,
             DEFAULT_TPU_CONNECTION_POOL_SIZE,
-            true,
+            DEFAULT_TPU_ENABLE_UDP,
             32, // max connections per IpAddr per minute
             Arc::new(RwLock::new(None)),
         )
