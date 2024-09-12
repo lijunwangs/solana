43112d8e14ead08a96cd6295a7de31cc56d52261
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-09-12 00:49:35 -0700

    fix a log format style

--- core/src/repair/quic_endpoint.rs
+++ core/src/repair/quic_endpoint.rs
@@ -249,10 +249,7 @@ async fn run_server(
                 ));
             }
             Err(error) => {
-                debug!(
-                    "Error while accepting incoming connection: {error:?} from {}",
-                    remote_addr
-                );
+                debug!("Error while accepting incoming connection: {error:?} from {remote_addr}");
             }
         }
     }
