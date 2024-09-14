14c801ed6f4b003e1e9aaadf4c4e6a0a976215e1
Lijun Wang <lijun@anza.xyz>
2024-09-14 09:17:47 +0000

    small stream parameters

--- sdk/src/quic.rs
+++ sdk/src/quic.rs
@@ -6,8 +6,8 @@ pub const QUIC_PORT_OFFSET: u16 = 6;
 // Empirically found max number of concurrent streams
 // that seems to maximize TPS on GCE (higher values don't seem to
 // give significant improvement or seem to impact stability)
-pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 128;
-pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 128;
+pub const QUIC_MAX_UNSTAKED_CONCURRENT_STREAMS: usize = 1;
+pub const QUIC_MIN_STAKED_CONCURRENT_STREAMS: usize = 1;
 
 pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
 
@@ -15,7 +15,7 @@ pub const QUIC_TOTAL_STAKED_CONCURRENT_STREAMS: usize = 100_000;
 // The value was lowered from 2048 to reduce contention of the limited
 // receive_window among the streams which is observed in CI bench-tests with
 // forwarded packets from staked nodes.
-pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 512;
+pub const QUIC_MAX_STAKED_CONCURRENT_STREAMS: usize = 1;
 
 pub const QUIC_MAX_TIMEOUT: Duration = Duration::from_secs(2);
 pub const QUIC_KEEP_ALIVE: Duration = Duration::from_secs(1);
@@ -27,15 +27,15 @@ pub const QUIC_CONNECTION_HANDSHAKE_TIMEOUT: Duration = Duration::from_secs(60);
 
 /// The receive window for QUIC connection from unstaked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
+pub const QUIC_UNSTAKED_RECEIVE_WINDOW_RATIO: u64 = 1;
 
 /// The receive window for QUIC connection from minimum staked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_MIN_STAKED_RECEIVE_WINDOW_RATIO: u64 = 128;
+pub const QUIC_MIN_STAKED_RECEIVE_WINDOW_RATIO: u64 = 1;
 
 /// The receive window for QUIC connection from maximum staked nodes is
 /// set to this ratio times [`solana_sdk::packet::PACKET_DATA_SIZE`]
-pub const QUIC_MAX_STAKED_RECEIVE_WINDOW_RATIO: u64 = 512;
+pub const QUIC_MAX_STAKED_RECEIVE_WINDOW_RATIO: u64 = 1;
 
 pub trait NotifyKeyUpdate {
     fn update_key(&self, key: &Keypair) -> Result<(), Box<dyn std::error::Error>>;
