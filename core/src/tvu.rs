e273d87a29961581404238fac61b65ed1ff3d977
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-09-21 02:50:36 -0700

    support notify first shred received in geyser

--- core/src/tvu.rs
+++ core/src/tvu.rs
@@ -25,7 +25,7 @@ use {
     bytes::Bytes,
     crossbeam_channel::{unbounded, Receiver, Sender},
     solana_client::connection_cache::ConnectionCache,
-    solana_geyser_plugin_manager::block_metadata_notifier_interface::BlockMetadataNotifierArc,
+    solana_geyser_plugin_manager::{block_metadata_notifier_interface::BlockMetadataNotifierArc, slot_status_notifier::SlotStatusNotifier},
     solana_gossip::{
         cluster_info::ClusterInfo, duplicate_shred_handler::DuplicateShredHandler,
         duplicate_shred_listener::DuplicateShredListener,
@@ -161,6 +161,7 @@ impl Tvu {
         outstanding_repair_requests: Arc<RwLock<OutstandingShredRepairs>>,
         cluster_slots: Arc<ClusterSlots>,
         wen_restart_repair_slots: Option<Arc<RwLock<Vec<Slot>>>>,
+        slot_status_notifier: Option<SlotStatusNotifier>,
     ) -> Result<Self, String> {
         let in_wen_restart = wen_restart_repair_slots.is_some();
 
@@ -210,6 +211,7 @@ impl Tvu {
             retransmit_receiver,
             max_slots.clone(),
             Some(rpc_subscriptions.clone()),
+            slot_status_notifier,
         );
 
         let (ancestor_duplicate_slots_sender, ancestor_duplicate_slots_receiver) = unbounded();
