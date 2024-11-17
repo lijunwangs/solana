c7b569b442b0c25f6f7c84ae2282ffc26152169c
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-11-16 19:06:19 -0800

    enable loggings

--- vortexor/src/main.rs
+++ vortexor/src/main.rs
@@ -1,10 +1,11 @@
 use {
-    clap::{value_t, value_t_or_exit, values_t},
+    clap::{crate_name, value_t, value_t_or_exit, values_t},
     crossbeam_channel::unbounded,
+    log::*,
     solana_clap_utils::input_parsers::keypair_of,
     solana_core::banking_trace::BankingTracer,
     solana_net_utils::{bind_in_range_with_config, SocketConfig},
-    solana_sdk::net::DEFAULT_TPU_COALESCE,
+    solana_sdk::{net::DEFAULT_TPU_COALESCE, signer::Signer},
     solana_streamer::streamer::StakedNodes,
     solana_vortexor::{
         cli::{app, DefaultArgs},
@@ -16,12 +17,80 @@ use {
     },
     std::{
         collections::HashSet,
+        env,
+        process::exit,
         sync::{atomic::AtomicBool, Arc, RwLock},
+        thread::JoinHandle,
         time::Duration,
     },
 };
 
+#[cfg(unix)]
+fn redirect_stderr(filename: &str) {
+    use std::{fs::OpenOptions, os::unix::io::AsRawFd};
+    match OpenOptions::new().create(true).append(true).open(filename) {
+        Ok(file) => unsafe {
+            libc::dup2(file.as_raw_fd(), libc::STDERR_FILENO);
+        },
+        Err(err) => eprintln!("Unable to open {filename}: {err}"),
+    }
+}
+
+// Redirect stderr to a file with support for logrotate by sending a SIGUSR1 to the process.
+//
+// Upon success, future `log` macros and `eprintln!()` can be found in the specified log file.
+pub fn redirect_stderr_to_file(logfile: Option<String>) -> Option<JoinHandle<()>> {
+    // Default to RUST_BACKTRACE=1 for more informative validator logs
+    if env::var_os("RUST_BACKTRACE").is_none() {
+        env::set_var("RUST_BACKTRACE", "1")
+    }
+
+    match logfile {
+        None => {
+            solana_logger::setup_with_default_filter();
+            None
+        }
+        Some(logfile) => {
+            #[cfg(unix)]
+            {
+                use log::info;
+                let mut signals =
+                    signal_hook::iterator::Signals::new([signal_hook::consts::SIGUSR1])
+                        .unwrap_or_else(|err| {
+                            eprintln!("Unable to register SIGUSR1 handler: {err:?}");
+                            exit(1);
+                        });
+
+                solana_logger::setup_with_default_filter();
+                redirect_stderr(&logfile);
+                Some(
+                    std::thread::Builder::new()
+                        .name("solSigUsr1".into())
+                        .spawn(move || {
+                            for signal in signals.forever() {
+                                info!(
+                                    "received SIGUSR1 ({}), reopening log file: {:?}",
+                                    signal, logfile
+                                );
+                                redirect_stderr(&logfile);
+                            }
+                        })
+                        .unwrap(),
+                )
+            }
+            #[cfg(not(unix))]
+            {
+                println!("logrotate is not supported on this platform");
+                solana_logger::setup_file_with_default(&logfile, solana_logger::DEFAULT_FILTER);
+                None
+            }
+        }
+    }
+}
+
 pub fn main() {
+    solana_logger::setup();
+
     let default_args = DefaultArgs::default();
     let solana_version = solana_version::version!();
     let cli_app = app(solana_version, &default_args);
@@ -34,6 +103,24 @@ pub fn main() {
         )
         .exit();
     });
+
+    let logfile = {
+        let logfile = matches
+            .value_of("logfile")
+            .map(|s| s.into())
+            .unwrap_or_else(|| format!("solana-vortexor-{}.log", identity_keypair.pubkey()));
+
+        if logfile == "-" {
+            None
+        } else {
+            println!("log file: {logfile}");
+            Some(logfile)
+        }
+    };
+    let _logger_thread = redirect_stderr_to_file(logfile);
+
+    info!("{} {}", crate_name!(), solana_version);
+    info!("Starting vortexor with: {:#?}", std::env::args_os());
 
     let bind_address = solana_net_utils::parse_host(matches.value_of("bind_address").unwrap())
         .expect("invalid bind_address");
@@ -68,7 +155,7 @@ pub fn main() {
     let sender_socket =
         bind_in_range_with_config(bind_address, dynamic_port_range, config).unwrap();
 
-    // The _non_vote_receiver will forward the verified transactions to its configured validator
+    // The non_vote_receiver will forward the verified transactions to its configured validator
     let (non_vote_sender, non_vote_receiver) = banking_tracer.create_channel_non_vote();
 
     let destinations = values_t!(matches, "destination", String)
@@ -83,6 +170,9 @@ pub fn main() {
         .into_iter()
         .collect::<Vec<_>>();
 
+    info!("Creating the PacketBatchSender: at address: {:?} for the following initial destinations: {destinations:?}",
+        sender_socket.1.local_addr());
+
     let destinations = Arc::new(RwLock::new(destinations));
     let packet_sender = PacketBatchSender::new(
         sender_socket.1,
@@ -93,10 +183,17 @@ pub fn main() {
         destinations,
     );
 
+    info!("Creating the SigVerifier");
     let sigverify_stage = Vortexor::create_sigverify_stage(tpu_receiver, non_vote_sender);
 
     // To be linked with StakedNodes service.
     let staked_nodes = Arc::new(RwLock::new(StakedNodes::default()));
+
+    info!(
+        "Creating the Vortexor. The tpu socket is: {:?}, tpu_fwd: {:?}",
+        tpu_sockets.tpu_quic[0].local_addr(),
+        tpu_sockets.tpu_quic_fwd[0].local_addr()
+    );
 
     let vortexor = Vortexor::create_vortexor(
         tpu_sockets,
