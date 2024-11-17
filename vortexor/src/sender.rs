700c5131f99cb6b54d5fe39b9356dcb5c2f06aa3
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-11-16 18:17:33 -0800

    Added initial implementations for packet forwarder
    clippy problems
    
    enable loggings
    
    reverted a change in cargo.toml in streamer
    
    log vortexor pubkey in log file

---
+++ vortexor/src/sender.rs
@@ -0,0 +1,136 @@
+//! Module responsible for sending verified transactions out to the registered
+//! validators
+
+use {
+    agave_banking_stage_ingress_types::{BankingPacketBatch, BankingPacketReceiver},
+    crossbeam_channel::RecvTimeoutError,
+    log::trace,
+    solana_streamer::sendmmsg::batch_send,
+    std::{
+        net::{SocketAddr, UdpSocket},
+        sync::{Arc, RwLock},
+        thread::{self, Builder, JoinHandle},
+        time::{Duration, Instant},
+    },
+};
+
+pub struct PacketBatchSender {
+    thread_hdls: Vec<JoinHandle<()>>,
+}
+
+pub const DEFAULT_SENDER_THREADS_COUNT: usize = 8;
+pub const DEFAULT_BATCH_SIZE: usize = 128;
+
+pub const DEFAULT_RECV_TIMEOUT: Duration = Duration::from_millis(100);
+
+impl PacketBatchSender {
+    pub fn new(
+        send_sock: UdpSocket,
+        packet_batch_receiver: BankingPacketReceiver,
+        num_threads: usize,
+        batch_size: usize,
+        recv_timeout: Duration,
+        destinations: Arc<RwLock<Vec<SocketAddr>>>,
+    ) -> Self {
+        let thread_hdls = (0..num_threads)
+            .map(|thread_id| {
+                let packet_batch_receiver = packet_batch_receiver.clone();
+                let destinations = destinations.clone();
+                let send_sock = send_sock.try_clone().unwrap();
+                // let recv_timeout = recv_timeout.clone();
+                Builder::new()
+                    .name(format!("vtxSdr{thread_id}"))
+                    .spawn(move || {
+                        Self::recv_send(
+                            send_sock,
+                            packet_batch_receiver,
+                            recv_timeout,
+                            batch_size,
+                            destinations,
+                        );
+                    })
+                    .unwrap()
+            })
+            .collect();
+
+        Self { thread_hdls }
+    }
+
+    pub fn join(self) -> thread::Result<()> {
+        for thread_hdl in self.thread_hdls {
+            thread_hdl.join()?;
+        }
+        Ok(())
+    }
+
+    fn recv_send(
+        send_sock: UdpSocket,
+        packet_batch_receiver: BankingPacketReceiver,
+        recv_timeout: Duration,
+        batch_size: usize,
+        destinations: Arc<RwLock<Vec<SocketAddr>>>,
+    ) {
+        loop {
+            match Self::receive_until(packet_batch_receiver.clone(), recv_timeout, batch_size) {
+                Ok((_packet_count, packet_batches)) => {
+                    // Send out packet batches
+
+                    let destinations = destinations.read().unwrap();
+                    for destination in destinations.iter() {
+                        let mut packets: Vec<(&[u8], &SocketAddr)> = Vec::new();
+
+                        packet_batches.iter().for_each(|batch| {
+                            batch.iter().for_each(|packet_batch| {
+                                for packet in packet_batch.iter() {
+                                    packets.push((packet.data(0..).unwrap(), destination));
+                                }
+                            });
+                        });
+                        let _result = batch_send(&send_sock, packets.into_iter());
+                    }
+                }
+                Err(err) => match err {
+                    RecvTimeoutError::Timeout => {
+                        continue;
+                    }
+                    RecvTimeoutError::Disconnected => {
+                        break;
+                    }
+                },
+            }
+        }
+    }
+
+    /// Receives packet batches from sigverify stage with a timeout
+    fn receive_until(
+        packet_batch_receiver: BankingPacketReceiver,
+        recv_timeout: Duration,
+        batch_size: usize,
+    ) -> Result<(usize, Vec<BankingPacketBatch>), RecvTimeoutError> {
+        let start = Instant::now();
+
+        let message = packet_batch_receiver.recv_timeout(recv_timeout)?;
+        let packet_batches = &message;
+        let mut num_packets_received = packet_batches
+            .iter()
+            .map(|batch| batch.len())
+            .sum::<usize>();
+        let mut messages = vec![message];
+
+        while let Ok(message) = packet_batch_receiver.try_recv() {
+            let packet_batches = &message;
+            trace!("got more packet batches in packet receiver");
+            num_packets_received += packet_batches
+                .iter()
+                .map(|batch| batch.len())
+                .sum::<usize>();
+            messages.push(message);
+
+            if start.elapsed() >= recv_timeout || num_packets_received >= batch_size {
+                break;
+            }
+        }
+
+        Ok((num_packets_received, messages))
+    }
+}
