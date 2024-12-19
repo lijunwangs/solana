803740422977bc584ca263f58573259d14a26e6b
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2024-12-19 01:44:17 -0800

    set max_unstaked_connections to 0 for tpu-fwd and vote in testing

--- core/src/validator.rs
+++ core/src/validator.rs
@@ -494,15 +494,19 @@ pub struct ValidatorTpuConfig {
     pub vote_quic_server_config: QuicServerParams,
 }
 
+/// A convenient function to build a ValidatorTpuConfig for testing with good
+/// default.
 pub fn build_validator_tpu_config_for_test(tpu_enable_udp: bool) -> ValidatorTpuConfig {
     let mut tpu_quic_server_config = QuicServerParams::default();
     tpu_quic_server_config.max_connections_per_ipaddr_per_min = 32;
 
     let mut tpu_fwd_quic_server_config = QuicServerParams::default();
     tpu_fwd_quic_server_config.max_connections_per_ipaddr_per_min = 32;
+    tpu_fwd_quic_server_config.max_unstaked_connections = 0;
 
     let mut vote_quic_server_config = QuicServerParams::default();
     vote_quic_server_config.max_connections_per_ipaddr_per_min = 32;
+    vote_quic_server_config.max_unstaked_connections = 0;
 
     ValidatorTpuConfig {
         use_quic: DEFAULT_TPU_USE_QUIC,
