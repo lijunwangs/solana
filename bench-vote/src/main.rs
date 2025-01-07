1b90cab95428ff318965e5ceedc7796e2139fdfc
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-01-06 18:29:15 -0800

    changed connection pool size to 512 from 256

--- bench-vote/src/main.rs
+++ bench-vote/src/main.rs
@@ -351,7 +351,7 @@ fn producer(
         if let Some(quic_params) = &quic_params {
             Transporter::Cache(Arc::new(ConnectionCache::new_with_client_options(
                 "connection_cache_vote_quic",
-                256,  // connection_pool_size
+                512,  // connection_pool_size
                 None, // client_endpoint
                 Some((
                     &quic_params.identity_keypair,
