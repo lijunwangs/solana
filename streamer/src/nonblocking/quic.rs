a7e521179574d02148dcd7b87671aae87b23f10f
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-01-15 20:42:43 -0800

    Revert "use unbounded"
    This reverts commit 921dfe50d4a39c1e57297f72f5e0bdcc4a666283.

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -10,7 +10,7 @@ use {
         quic::{configure_server, QuicServerError, QuicServerParams, StreamerStats},
         streamer::StakedNodes,
     },
-    async_channel::{unbounded as async_unbounded, Receiver as AsyncReceiver, Sender as AsyncSender},
+    async_channel::{bounded as async_bounded, Receiver as AsyncReceiver, Sender as AsyncSender},
     bytes::Bytes,
     crossbeam_channel::Sender,
     futures::{stream::FuturesUnordered, Future, StreamExt as _},
@@ -307,7 +307,7 @@ async fn run_server(
         .store(endpoints.len(), Ordering::Relaxed);
     let staked_connection_table: Arc<Mutex<ConnectionTable>> =
         Arc::new(Mutex::new(ConnectionTable::new()));
-    let (sender, receiver) = async_unbounded(/*MAX_COALESCE_CHANNEL_SIZE*/);
+    let (sender, receiver) = async_bounded(MAX_COALESCE_CHANNEL_SIZE);
     tokio::spawn(packet_batch_sender(
         packet_sender,
         receiver,
