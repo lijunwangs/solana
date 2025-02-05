6436be0c4728481a2b240d24ba51e85a22f9b51c
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-02-05 11:17:28 -0800

    output path on remove

--- accounts-db/src/append_vec.rs
+++ accounts-db/src/append_vec.rs
@@ -316,6 +316,7 @@ impl Drop for AppendVec {
         if self.remove_file_on_drop.load(Ordering::Acquire) {
             // If we're reopening in readonly mode, we don't delete the file. See
             // AppendVec::reopen_as_readonly.
+            info!("Removing appendvev file {:?}", self.path);
             if let Err(_err) = remove_file(&self.path) {
                 // promote this to panic soon.
                 // disabled due to many false positive warnings while running tests.
