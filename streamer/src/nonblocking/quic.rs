e1306ea0cb7ea002827c8c50ae458cb2f57de9a7
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-02-05 15:28:26 -0800

    added log for validator exit

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -441,7 +441,7 @@ async fn run_server(
             debug!("accept(): Timed out waiting for connection");
         }
     }
-    info!("zzzzz quic server {:?}", endpoints[0].local_addr());
+    info!("zzzzz quic server {:?} {}", endpoints[0].local_addr(), name);
 }
 
 fn prune_unstaked_connection_table(
