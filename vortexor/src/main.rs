d1cda793e89f518171849bd46b5738a51a4f3575
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-02-26 15:56:46 -0800

    refactor redirect_stderr_to_file into solana-log-utils

--- vortexor/src/main.rs
+++ vortexor/src/main.rs
@@ -4,6 +4,7 @@ use {
     log::*,
     solana_clap_utils::input_parsers::keypair_of,
     solana_core::banking_trace::BankingTracer,
+    solana_log_utils::redirect_stderr_to_file,
     solana_net_utils::{bind_in_range_with_config, SocketConfig},
     solana_sdk::{net::DEFAULT_TPU_COALESCE, signer::Signer},
     solana_streamer::streamer::StakedNodes,
@@ -19,73 +20,9 @@ use {
         collections::HashSet,
         env,
         sync::{atomic::AtomicBool, Arc, RwLock},
-        thread::JoinHandle,
         time::Duration,
     },
 };
-
-#[cfg(unix)]
-fn redirect_stderr(filename: &str) {
-    use std::{fs::OpenOptions, os::unix::io::AsRawFd};
-    match OpenOptions::new().create(true).append(true).open(filename) {
-        Ok(file) => unsafe {
-            libc::dup2(file.as_raw_fd(), libc::STDERR_FILENO);
-        },
-        Err(err) => eprintln!("Unable to open {filename}: {err}"),
-    }
-}
-
-// Redirect stderr to a file with support for logrotate by sending a SIGUSR1 to the process.
-//
-// Upon success, future `log` macros and `eprintln!()` can be found in the specified log file.
-pub fn redirect_stderr_to_file(logfile: Option<String>) -> Option<JoinHandle<()>> {
-    // Default to RUST_BACKTRACE=1 for more informative validator logs
-    if env::var_os("RUST_BACKTRACE").is_none() {
-        env::set_var("RUST_BACKTRACE", "1")
-    }
-
-    match logfile {
-        None => {
-            solana_logger::setup_with_default_filter();
-            None
-        }
-        Some(logfile) => {
-            #[cfg(unix)]
-            {
-                use log::info;
-                let mut signals =
-                    signal_hook::iterator::Signals::new([signal_hook::consts::SIGUSR1])
-                        .unwrap_or_else(|err| {
-                            eprintln!("Unable to register SIGUSR1 handler: {err:?}");
-                            std::process::exit(1);
-                        });
-
-                solana_logger::setup_with_default_filter();
-                redirect_stderr(&logfile);
-                Some(
-                    std::thread::Builder::new()
-                        .name("solSigUsr1".into())
-                        .spawn(move || {
-                            for signal in signals.forever() {
-                                info!(
-                                    "received SIGUSR1 ({}), reopening log file: {:?}",
-                                    signal, logfile
-                                );
-                                redirect_stderr(&logfile);
-                            }
-                        })
-                        .unwrap(),
-                )
-            }
-            #[cfg(not(unix))]
-            {
-                println!("logrotate is not supported on this platform");
-                solana_logger::setup_file_with_default(&logfile, solana_logger::DEFAULT_FILTER);
-                None
-            }
-        }
-    }
-}
 
 const DEFAULT_CHANNEL_SIZE: usize = 100_000;
 
