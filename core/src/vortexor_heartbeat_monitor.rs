be77514f17eb8d89f9c278832c668304175dcb7c
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-06-08 18:26:09 -0700

    Name the thread for monitor

--- core/src/vortexor_heartbeat_monitor.rs
+++ core/src/vortexor_heartbeat_monitor.rs
@@ -53,27 +53,30 @@ impl HeartbeatMonitor {
         tpu_switch: Arc<RwLock<TpuSwitch>>,
         heartbeat_receiver: Receiver<HeartbeatMessage>,
     ) -> JoinHandle<()> {
-        thread::spawn(move || {
-            let mut last_heartbeat = Instant::now();
+        thread::Builder::new()
+            .name("vtxHrtbtMtr".to_string())
+            .spawn(move || {
+                let mut last_heartbeat = Instant::now();
 
-            loop {
-                match heartbeat_receiver.recv_timeout(timeout) {
-                    Ok(_) => {
-                        // Received a heartbeat, update the last heartbeat timestamp.
-                        last_heartbeat = Instant::now();
-                        info!("Heartbeat received from vortexor. Resetting timeout.",);
-                        HeartbeatMonitor::switch_to_vortexor(tpu_switch.clone());
-                    }
-                    Err(_) => {
-                        // Timeout occurred, trigger the fallback action.
-                        if last_heartbeat.elapsed() >= timeout {
-                            error!("Heartbeat timeout detected. Triggering fallback action.");
-                            HeartbeatMonitor::switch_to_navite_tpu(tpu_switch.clone());
+                loop {
+                    match heartbeat_receiver.recv_timeout(timeout) {
+                        Ok(_) => {
+                            // Received a heartbeat, update the last heartbeat timestamp.
+                            last_heartbeat = Instant::now();
+                            info!("Heartbeat received from vortexor. Resetting timeout.",);
+                            HeartbeatMonitor::switch_to_vortexor(tpu_switch.clone());
+                        }
+                        Err(_) => {
+                            // Timeout occurred, trigger the fallback action.
+                            if last_heartbeat.elapsed() >= timeout {
+                                error!("Heartbeat timeout detected. Triggering fallback action.");
+                                HeartbeatMonitor::switch_to_navite_tpu(tpu_switch.clone());
+                            }
                         }
                     }
                 }
-            }
-        })
+            })
+            .unwrap()
     }
 
     pub fn join(self) -> Result<()> {
