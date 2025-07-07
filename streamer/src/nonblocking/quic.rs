b8b4dd54220180a673a4023d948806020addf0a6
Lijun Wang <83639177+lijunwangs@users.noreply.github.com>
2025-07-06 19:34:04 -0700

    clean up debug messages

--- streamer/src/nonblocking/quic.rs
+++ streamer/src/nonblocking/quic.rs
@@ -287,6 +287,7 @@ async fn run_server(
         TotalConnectionRateLimiter::new(TOTAL_CONNECTIONS_PER_SECOND);
 
     const WAIT_FOR_CONNECTION_TIMEOUT: Duration = Duration::from_secs(1);
+    debug!("spawn quic server");
     let mut last_datapoint = Instant::now();
     let unstaked_connection_table: Arc<Mutex<ConnectionTable>> =
         Arc::new(Mutex::new(ConnectionTable::new()));
