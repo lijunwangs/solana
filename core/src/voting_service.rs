fahimahmedx/alpenglow

Add option to push BLS message to Alpenglow sockets in voting service. (#207)
Co-authored-by: ksn6 <2163784+ksn6@users.noreply.github.com>

core/src/voting_service.rs
@@ -5,6 +5,7 @@ use {
         next_leader::upcoming_leader_tpu_vote_sockets,
         staked_validators_cache::StakedValidatorsCache,
     },
+    alpenglow_vote::bls_message::BLSMessage,
     bincode::serialize,
     crossbeam_channel::Receiver,
     solana_client::connection_cache::ConnectionCache,
@@ -36,8 +37,14 @@ pub enum VoteOp {
         tower_slots: Vec<Slot>,
         saved_tower: SavedTowerVersions,
     },
+    //TODO(wen): remove PushAlpenglowVote when BLS all to all is submmitted.
     PushAlpenglowVote {
         tx: Transaction,
+        slot: Slot,
+        saved_vote_history: SavedVoteHistoryVersions,
+    },
+    PushAlpenglowBLSMessage {
+        bls_message: BLSMessage,
         slot: Slot,
         saved_vote_history: SavedVoteHistoryVersions,
     },
@@ -55,6 +62,16 @@ enum SendVoteError {
     InvalidTpuAddress,
     #[error(transparent)]
     TransportError(#[from] TransportError),
+}
+
+fn send_message(
+    buf: Vec<u8>,
+    socket: &SocketAddr,
+    connection_cache: &Arc<ConnectionCache>,
+) -> Result<(), TransportError> {
+    let client = connection_cache.get_connection(socket);
+
+    client.send_data_async(buf)
 }
 
 fn send_vote_transaction(
@@ -160,6 +177,8 @@ impl VotingService {
         }
     }
 
+    // TODO(wen): broadcast_alpenglow_vote should be removed when all Alpenglow
+    // votes are sent through BLS messages.
     fn broadcast_alpenglow_vote(
         slot: Slot,
         cluster_info: &ClusterInfo,
@@ -169,7 +188,7 @@ impl VotingService {
         staked_validators_cache: &mut StakedValidatorsCache,
     ) {
         let (staked_validator_tpu_sockets, _) = staked_validators_cache
-            .get_staked_validators_by_slot(slot, cluster_info, Instant::now());
+            .get_staked_validators_by_slot_with_tpu_vote_ports(slot, cluster_info, Instant::now());
 
         if staked_validator_tpu_sockets.is_empty() {
             let _ = send_vote_transaction(cluster_info, tx, None, &connection_cache);
@@ -186,6 +205,43 @@ impl VotingService {
                     tx,
                     Some(*tpu_vote_socket),
                     &connection_cache,
+                );
+            }
+        }
+    }
+
+    fn broadcast_alpenglow_message(
+        slot: Slot,
+        cluster_info: &ClusterInfo,
+        bls_message: &BLSMessage,
+        connection_cache: Arc<ConnectionCache>,
+        additional_listeners: Option<&Vec<SocketAddr>>,
+        staked_validators_cache: &mut StakedValidatorsCache,
+    ) {
+        let (staked_validator_alpenglow_sockets, _) = staked_validators_cache
+            .get_staked_validators_by_slot_with_alpenglow_ports(slot, cluster_info, Instant::now());
+
+        let sockets = additional_listeners
+            .map(|v| v.as_slice())
+            .unwrap_or(&[])
+            .iter()
+            .chain(staked_validator_alpenglow_sockets.iter());
+        let buf = match serialize(bls_message) {
+            Ok(buf) => buf,
+            Err(err) => {
+                error!("Failed to serialize alpenglow message: {:?}", err);
+                return;
+            }
+        };
+
+        // We use send_message in a loop right now because we worry that sending packets too fast
+        // will cause a packet spike and overwhelm the network. If we later find out that this is
+        // not an issue, we can optimize this by using multi_targret_send or similar methods.
+        for alpenglow_socket in sockets {
+            if let Err(e) = send_message(buf.clone(), alpenglow_socket, &connection_cache) {
+                warn!(
+                    "Failed to send alpenglow message to {}: {:?}",
+                    alpenglow_socket, e
                 );
             }
         }
@@ -244,6 +300,28 @@ impl VotingService {
                 // TODO: Test that no important votes are overwritten
                 cluster_info.push_alpenglow_vote(tx);
             }
+            VoteOp::PushAlpenglowBLSMessage {
+                bls_message,
+                slot,
+                saved_vote_history,
+            } => {
+                let mut measure = Measure::start("alpenglow vote history save");
+                if let Err(err) = vote_history_storage.store(&saved_vote_history) {
+                    error!("Unable to save vote history to storage: {:?}", err);
+                    std::process::exit(1);
+                }
+                measure.stop();
+                trace!("{measure}");
+
+                Self::broadcast_alpenglow_message(
+                    slot,
+                    cluster_info,
+                    &bls_message,
+                    connection_cache,
+                    additional_listeners,
+                    staked_validators_cache,
+                );
+            }
             VoteOp::RefreshVote {
                 tx,
                 last_voted_slot,
@@ -255,5 +333,147 @@ impl VotingService {
 
     pub fn join(self) -> thread::Result<()> {
         self.thread_hdl.join()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::{
+            alpenglow_consensus::vote_history_storage::{NullVoteHistoryStorage, SavedVoteHistory},
+            consensus::tower_storage::NullTowerStorage,
+        },
+        alpenglow_vote::{
+            bls_message::{BLSMessage, VoteMessage},
+            vote::Vote,
+        },
+        solana_bls::Signature as BLSSignature,
+        solana_gossip::{cluster_info::ClusterInfo, contact_info::ContactInfo},
+        solana_ledger::{
+            blockstore::Blockstore, get_tmp_ledger_path_auto_delete,
+            leader_schedule_cache::LeaderScheduleCache,
+        },
+        solana_poh_config::PohConfig,
+        solana_runtime::{
+            bank::Bank,
+            bank_forks::BankForks,
+            genesis_utils::{
+                create_genesis_config_with_alpenglow_vote_accounts_no_program,
+                ValidatorVoteKeypairs,
+            },
+        },
+        solana_sdk::{
+            hash::Hash,
+            signer::{keypair::Keypair, Signer},
+        },
+        solana_streamer::{
+            packet::{Packet, PacketBatch},
+            recvmmsg::recv_mmsg,
+            socket::SocketAddrSpace,
+        },
+        std::{
+            net::SocketAddr,
+            sync::{atomic::AtomicBool, Arc, RwLock},
+        },
+    };
+
+    fn create_voting_service(
+        vote_receiver: Receiver<VoteOp>,
+        listener: SocketAddr,
+    ) -> VotingService {
+        // Create 10 node validatorvotekeypairs vec
+        let validator_keypairs = (0..10)
+            .map(|_| ValidatorVoteKeypairs::new_rand())
+            .collect::<Vec<_>>();
+        let genesis = create_genesis_config_with_alpenglow_vote_accounts_no_program(
+            1_000_000_000,
+            &validator_keypairs,
+            vec![100; validator_keypairs.len()],
+        );
+        let bank0 = Bank::new_for_tests(&genesis.genesis_config);
+        let bank_forks = BankForks::new_rw_arc(bank0);
+        let keypair = Keypair::new();
+        let contact_info = ContactInfo::new_localhost(&keypair.pubkey(), 0);
+        let cluster_info = ClusterInfo::new(
+            contact_info,
+            Arc::new(keypair),
+            SocketAddrSpace::Unspecified,
+        );
+        let ledger_path = get_tmp_ledger_path_auto_delete!();
+        let blockstore = Blockstore::open(ledger_path.path())
+            .expect("Expected to be able to open database ledger");
+        let working_bank = bank_forks.read().unwrap().working_bank();
+        let poh_recorder = PohRecorder::new(
+            working_bank.tick_height(),
+            working_bank.last_blockhash(),
+            working_bank.clone(),
+            None,
+            working_bank.ticks_per_slot(),
+            Arc::new(blockstore),
+            &Arc::new(LeaderScheduleCache::new_from_bank(&working_bank)),
+            &PohConfig::default(),
+            Arc::new(AtomicBool::new(false)),
+        )
+        .0;
+
+        VotingService::new(
+            vote_receiver,
+            Arc::new(cluster_info),
+            Arc::new(RwLock::new(poh_recorder)),
+            Arc::new(NullTowerStorage::default()),
+            Arc::new(NullVoteHistoryStorage::default()),
+            Arc::new(ConnectionCache::with_udp("TestConnectionCache", 10)),
+            bank_forks,
+            Some(vec![listener]),
+        )
+    }
+
+    #[allow(clippy::disallowed_methods)]
+    #[test]
+    fn test_send_bls_message() {
+        solana_logger::setup();
+        let (vote_sender, vote_receiver) = crossbeam_channel::unbounded();
+        // Create listener thread on a random port we allocated and return SocketAddr to create VotingService
+
+        // Bind to a random UDP port
+        let socket = solana_net_utils::bind_to_localhost().unwrap();
+        let listener_addr = socket.local_addr().unwrap();
+
+        // Create VotingService with the listener address
+        let _ = create_voting_service(vote_receiver, listener_addr);
+
+        // Send a BLS message via the VotingService
+        let bls_message = BLSMessage::Vote(VoteMessage {
+            vote: Vote::new_notarization_vote(5, Hash::new_unique(), Hash::new_unique()),
+            signature: BLSSignature::default(),
+            rank: 1,
+        });
+        let saved_vote_history = SavedVoteHistoryVersions::Current(SavedVoteHistory::default());
+        assert!(vote_sender
+            .send(VoteOp::PushAlpenglowBLSMessage {
+                bls_message: bls_message.clone(),
+                slot: 5,
+                saved_vote_history,
+            })
+            .is_ok());
+
+        // Wait for the listener to receive the message
+        let mut packet_batch = PacketBatch::new(vec![Packet::default()]);
+        socket
+            .set_read_timeout(Some(Duration::from_secs(2)))
+            .unwrap();
+        assert!(recv_mmsg(&socket, &mut packet_batch[..]).is_ok());
+        let packet = packet_batch.iter().next().expect("No packets received");
+        let received_bls_message = packet
+            .deserialize_slice::<BLSMessage, _>(..)
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Failed to deserialize BLSMessage: {:?} {:?}",
+                    size_of::<BLSMessage>(),
+                    err
+                )
+            });
+        assert_eq!(received_bls_message, bls_message);
     }
 }

lijunwangs/solana

core/src/voting_service.rs
@@ -5,6 +5,7 @@ use {
         next_leader::upcoming_leader_tpu_vote_sockets,
         staked_validators_cache::StakedValidatorsCache,
     },
+    alpenglow_vote::bls_message::BLSMessage,
     bincode::serialize,
     crossbeam_channel::Receiver,
     solana_client::connection_cache::ConnectionCache,
@@ -34,8 +35,14 @@ pub enum VoteOp {
         tower_slots: Vec<Slot>,
         saved_tower: SavedTowerVersions,
     },
+    //TODO(wen): remove PushAlpenglowVote when BLS all to all is submmitted.
     PushAlpenglowVote {
         tx: Transaction,
+        slot: Slot,
+        saved_vote_history: SavedVoteHistoryVersions,
+    },
+    PushAlpenglowBLSMessage {
+        bls_message: BLSMessage,
         slot: Slot,
         saved_vote_history: SavedVoteHistoryVersions,
     },
@@ -53,6 +60,16 @@ enum SendVoteError {
     InvalidTpuAddress,
     #[error(transparent)]
     TransportError(#[from] TransportError),
+}
+
+fn send_message(
+    buf: Vec<u8>,
+    socket: &SocketAddr,
+    connection_cache: &Arc<ConnectionCache>,
+) -> Result<(), TransportError> {
+    let client = connection_cache.get_connection(socket);
+
+    client.send_data_async(buf)
 }
 
 fn send_vote_transaction(
@@ -158,6 +175,8 @@ impl VotingService {
         }
     }
 
+    // TODO(wen): broadcast_alpenglow_vote should be removed when all Alpenglow
+    // votes are sent through BLS messages.
     fn broadcast_alpenglow_vote(
         slot: Slot,
         cluster_info: &ClusterInfo,
@@ -167,7 +186,7 @@ impl VotingService {
         staked_validators_cache: &mut StakedValidatorsCache,
     ) {
         let (staked_validator_tpu_sockets, _) = staked_validators_cache
-            .get_staked_validators_by_slot(slot, cluster_info, Instant::now());
+            .get_staked_validators_by_slot_with_tpu_vote_ports(slot, cluster_info, Instant::now());
 
         if staked_validator_tpu_sockets.is_empty() {
             let _ = send_vote_transaction(cluster_info, tx, None, &connection_cache);
@@ -184,6 +203,43 @@ impl VotingService {
                     tx,
                     Some(*tpu_vote_socket),
                     &connection_cache,
+                );
+            }
+        }
+    }
+
+    fn broadcast_alpenglow_message(
+        slot: Slot,
+        cluster_info: &ClusterInfo,
+        bls_message: &BLSMessage,
+        connection_cache: Arc<ConnectionCache>,
+        additional_listeners: Option<&Vec<SocketAddr>>,
+        staked_validators_cache: &mut StakedValidatorsCache,
+    ) {
+        let (staked_validator_alpenglow_sockets, _) = staked_validators_cache
+            .get_staked_validators_by_slot_with_alpenglow_ports(slot, cluster_info, Instant::now());
+
+        let sockets = additional_listeners
+            .map(|v| v.as_slice())
+            .unwrap_or(&[])
+            .iter()
+            .chain(staked_validator_alpenglow_sockets.iter());
+        let buf = match serialize(bls_message) {
+            Ok(buf) => buf,
+            Err(err) => {
+                error!("Failed to serialize alpenglow message: {:?}", err);
+                return;
+            }
+        };
+
+        // We use send_message in a loop right now because we worry that sending packets too fast
+        // will cause a packet spike and overwhelm the network. If we later find out that this is
+        // not an issue, we can optimize this by using multi_targret_send or similar methods.
+        for alpenglow_socket in sockets {
+            if let Err(e) = send_message(buf.clone(), alpenglow_socket, &connection_cache) {
+                warn!(
+                    "Failed to send alpenglow message to {}: {:?}",
+                    alpenglow_socket, e
                 );
             }
         }
@@ -242,6 +298,28 @@ impl VotingService {
                 // TODO: Test that no important votes are overwritten
                 cluster_info.push_alpenglow_vote(tx);
             }
+            VoteOp::PushAlpenglowBLSMessage {
+                bls_message,
+                slot,
+                saved_vote_history,
+            } => {
+                let mut measure = Measure::start("alpenglow vote history save");
+                if let Err(err) = vote_history_storage.store(&saved_vote_history) {
+                    error!("Unable to save vote history to storage: {:?}", err);
+                    std::process::exit(1);
+                }
+                measure.stop();
+                trace!("{measure}");
+
+                Self::broadcast_alpenglow_message(
+                    slot,
+                    cluster_info,
+                    &bls_message,
+                    connection_cache,
+                    additional_listeners,
+                    staked_validators_cache,
+                );
+            }
             VoteOp::RefreshVote {
                 tx,
                 last_voted_slot,
@@ -253,5 +331,142 @@ impl VotingService {
 
     pub fn join(self) -> thread::Result<()> {
         self.thread_hdl.join()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use {
+        super::*,
+        crate::{
+            alpenglow_consensus::vote_history_storage::{NullVoteHistoryStorage, SavedVoteHistory},
+            consensus::tower_storage::NullTowerStorage,
+        },
+        alpenglow_vote::{
+            bls_message::{BLSMessage, VoteMessage},
+            vote::Vote,
+        },
+        solana_bls::Signature as BLSSignature,
+        solana_gossip::{cluster_info::ClusterInfo, contact_info::ContactInfo},
+        solana_hash::Hash,
+        solana_keypair::Keypair,
+        solana_ledger::{
+            blockstore::Blockstore, get_tmp_ledger_path_auto_delete,
+            leader_schedule_cache::LeaderScheduleCache,
+        },
+        solana_poh_config::PohConfig,
+        solana_runtime::{
+            bank::Bank,
+            bank_forks::BankForks,
+            genesis_utils::{
+                create_genesis_config_with_alpenglow_vote_accounts_no_program,
+                ValidatorVoteKeypairs,
+            },
+        },
+        solana_signer::Signer,
+        solana_streamer::{packet::Packet, recvmmsg::recv_mmsg, socket::SocketAddrSpace},
+        std::{
+            net::SocketAddr,
+            sync::{atomic::AtomicBool, Arc, RwLock},
+        },
+    };
+
+    fn create_voting_service(
+        vote_receiver: Receiver<VoteOp>,
+        listener: SocketAddr,
+    ) -> VotingService {
+        // Create 10 node validatorvotekeypairs vec
+        let validator_keypairs = (0..10)
+            .map(|_| ValidatorVoteKeypairs::new_rand())
+            .collect::<Vec<_>>();
+        let genesis = create_genesis_config_with_alpenglow_vote_accounts_no_program(
+            1_000_000_000,
+            &validator_keypairs,
+            vec![100; validator_keypairs.len()],
+        );
+        let bank0 = Bank::new_for_tests(&genesis.genesis_config);
+        let bank_forks = BankForks::new_rw_arc(bank0);
+        let keypair = Keypair::new();
+        let contact_info = ContactInfo::new_localhost(&keypair.pubkey(), 0);
+        let cluster_info = ClusterInfo::new(
+            contact_info,
+            Arc::new(keypair),
+            SocketAddrSpace::Unspecified,
+        );
+        let ledger_path = get_tmp_ledger_path_auto_delete!();
+        let blockstore = Blockstore::open(ledger_path.path())
+            .expect("Expected to be able to open database ledger");
+        let working_bank = bank_forks.read().unwrap().working_bank();
+        let poh_recorder = PohRecorder::new(
+            working_bank.tick_height(),
+            working_bank.last_blockhash(),
+            working_bank.clone(),
+            None,
+            working_bank.ticks_per_slot(),
+            Arc::new(blockstore),
+            &Arc::new(LeaderScheduleCache::new_from_bank(&working_bank)),
+            &PohConfig::default(),
+            Arc::new(AtomicBool::new(false)),
+        )
+        .0;
+
+        VotingService::new(
+            vote_receiver,
+            Arc::new(cluster_info),
+            Arc::new(RwLock::new(poh_recorder)),
+            Arc::new(NullTowerStorage::default()),
+            Arc::new(NullVoteHistoryStorage::default()),
+            Arc::new(ConnectionCache::with_udp("TestConnectionCache", 10)),
+            bank_forks,
+            Some(vec![listener]),
+        )
+    }
+
+    #[allow(clippy::disallowed_methods)]
+    #[test]
+    fn test_send_bls_message() {
+        solana_logger::setup();
+        let (vote_sender, vote_receiver) = crossbeam_channel::unbounded();
+        // Create listener thread on a random port we allocated and return SocketAddr to create VotingService
+
+        // Bind to a random UDP port
+        let socket = solana_net_utils::bind_to_localhost().unwrap();
+        let listener_addr = socket.local_addr().unwrap();
+
+        // Create VotingService with the listener address
+        let _ = create_voting_service(vote_receiver, listener_addr);
+
+        // Send a BLS message via the VotingService
+        let bls_message = BLSMessage::Vote(VoteMessage {
+            vote: Vote::new_notarization_vote(5, Hash::new_unique(), Hash::new_unique()),
+            signature: BLSSignature::default(),
+            rank: 1,
+        });
+        let saved_vote_history = SavedVoteHistoryVersions::Current(SavedVoteHistory::default());
+        assert!(vote_sender
+            .send(VoteOp::PushAlpenglowBLSMessage {
+                bls_message: bls_message.clone(),
+                slot: 5,
+                saved_vote_history,
+            })
+            .is_ok());
+
+        // Wait for the listener to receive the message
+        let mut packets = vec![Packet::default(); 1];
+        socket
+            .set_read_timeout(Some(Duration::from_secs(2)))
+            .unwrap();
+        assert!(recv_mmsg(&socket, &mut packets[..]).is_ok());
+        let packet = packets.first().expect("No packets received");
+        let received_bls_message = packet
+            .deserialize_slice::<BLSMessage, _>(..)
+            .unwrap_or_else(|err| {
+                panic!(
+                    "Failed to deserialize BLSMessage: {:?} {:?}",
+                    size_of::<BLSMessage>(),
+                    err
+                )
+            });
+        assert_eq!(received_bls_message, bls_message);
     }
 }