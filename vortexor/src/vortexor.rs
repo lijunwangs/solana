anza-xyz/agave

rebase with master

vortexor/src/vortexor.rs
@@ -12,7 +12,7 @@ use {
     solana_quic_definitions::NotifyKeyUpdate,
     solana_streamer::{
         nonblocking::quic::DEFAULT_WAIT_FOR_CHUNK_TIMEOUT,
-        quic::{spawn_server, EndpointKeyUpdater, QuicServerParams},
+        quic::{spawn_server, EndpointKeyUpdater, QosMode, QuicServerParams},
         streamer::StakedNodes,
     },
     std::{
@@ -121,10 +121,10 @@ impl Vortexor {
             max_connections_per_peer,
             max_staked_connections: max_tpu_staked_connections,
             max_unstaked_connections: max_tpu_unstaked_connections,
-            max_streams_per_ms,
             max_connections_per_ipaddr_per_min,
             wait_for_chunk_timeout: DEFAULT_WAIT_FOR_CHUNK_TIMEOUT,
             coalesce: tpu_coalesce,
+            qos_mode: QosMode::StakeWeighted { max_streams_per_ms },
             ..Default::default()
         };
 

lijunwangs/solana

vortexor/src/vortexor.rs
@@ -12,7 +12,7 @@ use {
     solana_quic_definitions::NotifyKeyUpdate,
     solana_streamer::{
         nonblocking::quic::DEFAULT_WAIT_FOR_CHUNK_TIMEOUT,
-        quic::{spawn_server_with_cancel, EndpointKeyUpdater, QuicServerParams},
+        quic::{spawn_server_with_cancel, EndpointKeyUpdater, QosMode, QuicServerParams},
         streamer::StakedNodes,
     },
     std::{
@@ -122,10 +122,10 @@ impl Vortexor {
             max_connections_per_peer,
             max_staked_connections: max_tpu_staked_connections,
             max_unstaked_connections: max_tpu_unstaked_connections,
-            max_streams_per_ms,
             max_connections_per_ipaddr_per_min,
             wait_for_chunk_timeout: DEFAULT_WAIT_FOR_CHUNK_TIMEOUT,
             coalesce: tpu_coalesce,
+            qos_mode: QosMode::StakeWeighted { max_streams_per_ms },
             ..Default::default()
         };
 